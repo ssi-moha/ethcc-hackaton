3b05d8a2df6aa3d67b5d7f435000c7fc8c731b75
arezgui <rezgui.amine-ext@urbasolar.com>
2022-07-22 16:11:22 +0200

    feat(ui): add chakra librairie

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import '@rainbow-me/rainbowkit/styles.css'
+import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiConfig } from "wagmi";
 import App from "./App";
 import { wagmiClient, chains } from "./clients/wagmi";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>
