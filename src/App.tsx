7ceb6a023e454aa3b84e46f099089cb32e452c33
Sinane <sin.sim@outlook.fr>
2022-07-22 19:02:40 +0200

    feat: set update theme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, HStack } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { Banner } from "./components/Banner";
 import { Layout } from "./components/Layout";
 import { NavBar } from "./components/NavBar";
+import useUpdateThemeOnConnection from "./hooks/useUpdateThemeOnConnection";
 import { RootState } from "./store/store";
 import { themes } from "./theme";
 
 function App() {
   const theme = useSelector((state: RootState) => state.theme);
-
+  const {} = useUpdateThemeOnConnection();
   return (
     <ChakraProvider theme={themes[theme]}>
       <Box>
@@ -17,6 +18,9 @@ function App() {
 
           <Banner />
         </Layout>
+        <Box mt={10} display="flex" justifyContent="center" alignItems="center">
+          {theme}
+        </Box>
       </Box>
     </ChakraProvider>
   );
