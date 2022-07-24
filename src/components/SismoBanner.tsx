084116228b159e45cbd7440ba413947abfa347fb
Sinane <sin.sim@outlook.fr>
2022-07-24 11:52:56 +0200

    feat: link to sismo badge

--- src/components/SismoBanner.tsx
+++ src/components/SismoBanner.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Link, Text } from "@chakra-ui/react";
 import SismoLogo from "../assets/sismo.jpeg";
 
 const SismoBanner = () => {
   return (
-    <HStack mt={2}>
-      <Image height={8} width={8} src={SismoLogo} alt="Sismo" />
-      <Text>Connect your wallet owning Sismo attestations</Text>
-    </HStack>
+    <Link href="https://sandbox.sismo.io/misfitwear" isExternal>
+      <HStack mt={2}>
+        <Image height={8} width={8} src={SismoLogo} alt="Sismo" />
+        <Text>Connect your wallet owning Sismo attestations</Text>
+      </HStack>
+    </Link>
   );
 };
 
